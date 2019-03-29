c1a347f2cebd32d214c1877e67880e63004200e0
Francis Lim//RainyIllusionist <16099962+rainyillusionist@users.noreply.github.com>
2019-03-30 01:14:16 +0800

    Fixed bugs pertaining to ReservationMenu; added Table.csv

--- src/main/java/sg/edu/ntu/scse/cz2002/features/Table.java
+++ src/main/java/sg/edu/ntu/scse/cz2002/features/Table.java
@@ -11,25 +11,49 @@ package sg.edu.ntu.scse.cz2002.features;
 public class Table {
 
     /**
+     * The states of the table, whether it has been reserved, occupied or vacated.
+     */
+    public enum TableState {TABLE_VACATED, TABLE_OCCUPIED, TABLE_RESERVED, TABLE_STATE_UNKNOWN};
+
+    /**
+     * The varying number of seats at different tables
+     */
+    private enum TableSeats {TEN_SEATER, EIGHT_SEATER, FOUR_SEATER, TWO_SEATER, UNKNOWN_SEATER};
+
+    /**
      * The number that is allocated to the table.
      */
     private int tableNum;
+
     /**
      *The state of whether the table is reserved.
      */
     private boolean isReserved;
 
     /**
-     * The states of the table, whether it has been reserved, occupied or vacated.
+     * State of the table
      */
-    public enum TableState {TABLE_VACATED, TABLE_OCCUPIED, TABLE_RESERVED};
+    private TableState state;
 
     /**
-     * The number of seats at the table
+     * Number of seats at the table
      */
-    private enum TableSeats {TEN_SEATER, EIGHT_SEATER, FOUR_SEATER, TWO_SEATER};
+    private TableSeats numSeats;
 
-    public Table () {}
+
+    public Table(int num, boolean res, int seats, int state) {
+        this.tableNum = num;
+        this.isReserved = res;
+        this.numSeats   = seats == 2 ? TableSeats.TWO_SEATER      :
+                            seats == 4 ? TableSeats.FOUR_SEATER   :
+                            seats == 8 ? TableSeats.EIGHT_SEATER  :
+                            seats == 10 ? TableSeats.TEN_SEATER   :
+                                     TableSeats.UNKNOWN_SEATER;
+        this.state      = state == 0 ? TableState.TABLE_OCCUPIED    :
+                            state == 1 ? TableState.TABLE_VACATED   :
+                            state == 2 ? TableState.TABLE_RESERVED  :
+                                         TableState.TABLE_STATE_UNKNOWN;
+    }
 
     /**
      * A method to check if a table has already been reserved
