051d1d53503e5ffcf255d4ab0f61393e5c90b506
Kenneth Soh <sohy0037@e.ntu.edu.sg>
2019-04-07 22:53:09 +0800

    Fixed wrong javadocs in ItemNotFoundException

--- src/main/java/sg/edu/ntu/scse/cz2002/objects/menuitem/ItemNotFoundException.java
+++ src/main/java/sg/edu/ntu/scse/cz2002/objects/menuitem/ItemNotFoundException.java
@@ -1,15 +1,26 @@
 package sg.edu.ntu.scse.cz2002.objects.menuitem;
 
 /**
- * Created by Kenneth on 4/4/2019.
- * for sg.edu.ntu.scse.cz2002.objects.menuitem in assignment-fsp6-grp2
+ * Exception when an Order Item cannot be found in the order
+ *
+ * @author Kenneth Soh
+ * @version 1.0
+ * @since 2019-04-04
  */
 public class ItemNotFoundException extends Exception {
 
+    /**
+     * Constructor for the exception
+     * @param message Message you wish to send as part of the exception
+     */
     public ItemNotFoundException(String message) {
         super(message);
     }
 
+    /**
+     * Prints the exception out
+     * @return Exception String
+     */
     @Override
     public String toString() {
         return super.toString() + ". Is it declared properly in the database?";
